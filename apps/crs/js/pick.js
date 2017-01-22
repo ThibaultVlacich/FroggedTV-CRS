33194ae4dbd81cceebf22edd1d7c2263f5573563
Thibault Vlacich <thibault.vlacich@gmail.com>
2017-01-22 21:26:27 +0100

    [Pick overlay] Use video var

--- apps/crs/js/pick.js
+++ apps/crs/js/pick.js
@@ -2,16 +2,16 @@ require(['jquery'], function ($) {
     var portrait_video_url = wity_base_url + '/apps/crs/front/video/portrait.mp4';
 
     $('.wity-app-crs.wity-app-crs-pick video').on('click', function(event) {
-        var video_url = wity_base_url + '/apps/crs/front/video/' + $(this).attr('data-portrait-video');
+        var video_url = wity_base_url + '/apps/crs/front/video/' + $(this).data('portrait-video');
 
-        if ($(this).attr('data-is-showed') == 'true') {
+        if ($(this).data('is-showed') == 'true') {
             $(this).attr('src', portrait_video_url);
 
-            $(this).attr('data-is-showed', 'false');
+            $(this).data('is-showed', 'false');
         } else {
             $(this).attr('src', video_url);
 
-            $(this).attr('data-is-showed', 'true');
+            $(this).data('is-showed', 'true');
         }
     });
 });
